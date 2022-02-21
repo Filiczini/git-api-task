3ba3ae401d0d9d71eeaf1d7f66b13227a2aaf4ce
Filiczini <51033321+Filiczini@users.noreply.github.com>
2022-02-21 15:56:48 +0200

    add username & repo search, show data in table

--- src/App.js
+++ src/App.js
@@ -1,13 +1,88 @@
+import React, { useState, useEffect } from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Container from "@mui/material/Container";
 import Header from "./components/header/header";
 import GitDataTable from "./components/data-table/git-data-table";
 
 function App() {
+  const rows = [
+    {
+      title:
+        "[Profile] Implement functionality to remove users avatar (Front End part)",
+      labels: ["feature", "fontEnd part", "UI"],
+      assignee: "SirSir",
+      comments: 3,
+    },
+    {
+      title:
+        "(SP:2) Save user configs to DB after logging out (Front End part)",
+      labels: ["data base", "feature", "fontEnd part"],
+      assignee: "Anastasia",
+      comments: 5,
+    },
+    {
+      title: "[Catalog] Make adaptive design for Catalog page",
+      labels: ["design", "fontEnd part", "responsive"],
+      assignee: "filiczini",
+      comments: 2,
+    },
+    {
+      title: "(SP:3) Refactor payment with Fondy",
+      labels: ["Functional", "priority: high"],
+      assignee: "Mark",
+      comments: 7,
+    },
+    {
+      title: "(SP:0.5) Investigate response from Fondy",
+      labels: ["fontEnd part", "Functional", "priority: high"],
+      assignee: "Oleh",
+      comments: 8,
+    },
+  ];
+
+  const [repoName, setRepoName] = useState("");
+  const [userName, setUserName] = useState("");
+  const [data, setData] = useState(rows);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(`https://api.github.com/repos/${userName}/${repoName}/issues`)
+      .then((res) => res.json())
+      .then((data) => {
+        return setData(data);
+      });
+  }
+  function handleUserName(e) {
+    setUserName(e.target.value);
+  }
+  function handleRepoName(e) {
+    setRepoName(e.target.value);
+  }
+
   return (
     <div className="App">
       <Header />
+
       <Container maxWidth="lg">
-        <GitDataTable />
+        <form>
+          <TextField
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+            onChange={handleUserName}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Repo name"
+            variant="outlined"
+            onChange={handleRepoName}
+          />
+          <Button type="submit" onClick={handleSubmit} variant="outlined">
+            Search
+          </Button>
+        </form>
+        <GitDataTable rows={data} />
       </Container>
     </div>
   );
--- src/components/data-table/git-data-table.js
+++ src/components/data-table/git-data-table.js
@@ -7,68 +7,31 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
-function createData(name, label, assignee, comments) {
-  return { name, label, assignee, comments };
-}
-
-const rows = [
-  createData(
-    "[Profile] Implement functionality to remove users avatar (Front End part)",
-    ["feature", "fontEnd part", "UI"],
-    "SirSir",
-    3
-  ),
-  createData(
-    "(SP:2) Save user configs to DB after logging out (Front End part)",
-    ["data base", "feature", "fontEnd part"],
-    "Anastasia",
-    5
-  ),
-  createData(
-    "[Catalog] Make adaptive design for Catalog page",
-    ["design", "fontEnd part", "responsive"],
-    "filiczini",
-    2
-  ),
-  createData(
-    "(SP:3) Refactor payment with Fondy",
-    ["Functional", "priority: high"],
-    "Mark",
-    7
-  ),
-  createData(
-    "(SP:0.5) Investigate response from Fondy",
-    ["fontEnd part", "Functional", "priority: high"],
-    "Oleh",
-    8
-  ),
-];
-
-export default function GitDataTable() {
+export default function GitDataTable(props) {
   return (
     <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+      <Table aria-label="git table">
         <TableHead>
           <TableRow>
             <TableCell>Issues</TableCell>
-            <TableCell align="right">Label</TableCell>
+            <TableCell align="right">Labels</TableCell>
             <TableCell align="right">Assignee</TableCell>
             <TableCell align="right">Comments</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-            >
+          {props.rows.map((row) => (
+            <TableRow key={row.title}>
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.title}
               </TableCell>
-              <TableCell align="right">{row.label}</TableCell>
-              <TableCell align="right">{row.assignee}</TableCell>
+              <TableCell align="right">
+                {row.labels.map((el) => el.name)}
+              </TableCell>
+              <TableCell align="right">
+                {row.assignee !== null ? row.assignee.login : "no assignees"}
+              </TableCell>
               <TableCell align="right">{row.comments}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
             </TableRow>
           ))}
         </TableBody>
