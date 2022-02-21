e9ffe51da09cd4f0b6e4c06bfcdf1bdddaccb92c
Filiczini <51033321+Filiczini@users.noreply.github.com>
2022-02-21 13:11:29 +0200

    initial commit

---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <img
+        src="https://www.freeiconspng.com/uploads/github-circle-mascot-git-icon-6.png"
+        alt="git octopus"
+      />
+
+      <span>Git API finder</span>
+    </header>
+  );
+}
+
+export default Header;
