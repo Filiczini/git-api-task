aa2aade98fb88f262269bc33ec4f3405066f79a5
Filiczini <51033321+Filiczini@users.noreply.github.com>
2022-02-21 17:29:25 +0200

    remove mocked data and add conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,51 +6,15 @@ import Header from "./components/header/header";
 import GitDataTable from "./components/data-table/git-data-table";
 
 function App() {
-  const rows = [
-    {
-      title:
-        "[Profile] Implement functionality to remove users avatar (Front End part)",
-      labels: ["feature", "fontEnd part", "UI"],
-      assignee: "SirSir",
-      comments: 3,
-    },
-    {
-      title:
-        "(SP:2) Save user configs to DB after logging out (Front End part)",
-      labels: ["data base", "feature", "fontEnd part"],
-      assignee: "Anastasia",
-      comments: 5,
-    },
-    {
-      title: "[Catalog] Make adaptive design for Catalog page",
-      labels: ["design", "fontEnd part", "responsive"],
-      assignee: "filiczini",
-      comments: 2,
-    },
-    {
-      title: "(SP:3) Refactor payment with Fondy",
-      labels: ["Functional", "priority: high"],
-      assignee: "Mark",
-      comments: 7,
-    },
-    {
-      title: "(SP:0.5) Investigate response from Fondy",
-      labels: ["fontEnd part", "Functional", "priority: high"],
-      assignee: "Oleh",
-      comments: 8,
-    },
-  ];
-
   const [repoName, setRepoName] = useState("");
   const [userName, setUserName] = useState("");
-  const [data, setData] = useState(rows);
+  const [data, setData] = useState([]);
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function handleSubmit() {
     fetch(`https://api.github.com/repos/${userName}/${repoName}/issues`)
       .then((res) => res.json())
-      .then((data) => {
-        return setData(data);
+      .then((fetchedData) => {
+        setData(fetchedData);
       });
   }
   function handleUserName(e) {
@@ -60,28 +24,48 @@ function App() {
     setRepoName(e.target.value);
   }
 
+
   return (
     <div className="App">
       <Header />
 
       <Container maxWidth="lg">
-        <form>
-          <TextField
-            id="outlined-basic"
-            label="Username"
-            variant="outlined"
-            onChange={handleUserName}
-          />
-          <TextField
-            id="outlined-basic"
-            label="Repo name"
-            variant="outlined"
-            onChange={handleRepoName}
-          />
-          <Button type="submit" onClick={handleSubmit} variant="outlined">
-            Search
-          </Button>
-        </form>
+        <div className="table-header">
+          <form>
+            <TextField
+              id="outlined-basic"
+              label="Username"
+              variant="outlined"
+              onChange={handleUserName}
+            />
+            <TextField
+              id="outlined-basic"
+              label="Repo name"
+              variant="outlined"
+              onChange={handleRepoName}
+            />
+            <Button onClick={handleSubmit} variant="outlined">
+              Search
+            </Button>
+          </form>
+          <div className="table-filtering">
+            <TextField
+              id="outlined-basic"
+              label="filter by label"
+              variant="outlined"
+              onChange={handleUserName}
+            />
+            <TextField
+              id="outlined-basic"
+              label="filter by assignee"
+              variant="outlined"
+            />
+            <Button onClick={null} variant="outlined">
+              SortByDate
+            </Button>
+          </div>
+        </div>
+
         <GitDataTable rows={data} />
       </Container>
     </div>
--- src/components/data-table/git-data-table.js
+++ src/components/data-table/git-data-table.js
@@ -8,6 +8,18 @@ import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 
 export default function GitDataTable(props) {
+  const dataRows = props.rows.map((row) => (
+    <TableRow key={row.title}>
+      <TableCell component="th" scope="row">
+        {row.title}
+      </TableCell>
+      <TableCell align="right">{row.labels.map((el) => el.name)}</TableCell>
+      <TableCell align="right">
+        {row.assignee !== null ? row.assignee.login : "no assignees"}
+      </TableCell>
+      <TableCell align="right">{row.comments}</TableCell>
+    </TableRow>
+  ));
   return (
     <TableContainer component={Paper}>
       <Table aria-label="git table">
@@ -20,20 +32,15 @@ export default function GitDataTable(props) {
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.rows.map((row) => (
-            <TableRow key={row.title}>
-              <TableCell component="th" scope="row">
-                {row.title}
+          {props.rows.length > 0 ? (
+            dataRows
+          ) : (
+            <TableRow>
+              <TableCell>
+                Please enter GitHub UserName and repository name
               </TableCell>
-              <TableCell align="right">
-                {row.labels.map((el) => el.name)}
-              </TableCell>
-              <TableCell align="right">
-                {row.assignee !== null ? row.assignee.login : "no assignees"}
-              </TableCell>
-              <TableCell align="right">{row.comments}</TableCell>
             </TableRow>
-          ))}
+          )}
         </TableBody>
       </Table>
     </TableContainer>
