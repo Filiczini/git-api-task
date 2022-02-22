6e453753365e124497e7313e7e590f7eab8455a5
Filiczini <51033321+Filiczini@users.noreply.github.com>
2022-02-22 11:59:00 +0200

    header add navbar

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -3,12 +3,20 @@ import "./header.css";
 function Header() {
   return (
     <header className="header">
-      <img
-        src="https://www.freeiconspng.com/uploads/github-circle-mascot-git-icon-6.png"
-        alt="git octopus"
-      />
-
-      <span>Git API finder</span>
+      <div className="container">
+        <div className="logo">
+          <img
+            src="https://www.freeiconspng.com/uploads/github-circle-mascot-git-icon-6.png"
+            alt="git octopus"
+          />
+        </div>
+        <nav className="navbar">
+          <h1>The Git API Searcher</h1>
+          <div className="links">
+            <a href="/">Home</a>
+          </div>
+        </nav>
+      </div>
     </header>
   );
 }
